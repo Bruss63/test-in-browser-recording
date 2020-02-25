ba8f4a65868ead258e95e4209f1cad582ef64d98
Bruss63 <56018925+Bruss63@users.noreply.github.com>
2020-02-25 15:39:29 +1000

    modulation

---
+++ in-browser-recording-frontend/src/AudioRecorder.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+
+function App (props) {
+	//props
+	
+	//Constants
+	const slice = 60000;
+	//States
+	const [mediaRecorder, setMediaRecorder] = useState(null);
+	const [stream, setStream] = useState(null);
+	
+	return {
+
+	}	
+}
