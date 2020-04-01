5ec39b8b9db579b970c28fdd19a13909efcb46f6
Bruss63 <bruss63@gmail.com>
2020-04-01 17:28:41 +1000

    improved matching, better formatting.

--- in-browser-recording-frontend/src/App.js
+++ in-browser-recording-frontend/src/App.js
@@ -13,81 +13,86 @@ function App() {
 	const SIGNED_URL_ENDPOINT =
 		"https://vf4q9rvdzb.execute-api.ap-southeast-2.amazonaws.com/dev/apps/signedURL";
 	const WEBSOCKET_URL =
-		"ws://ec2-54-245-174-184.us-west-2.compute.amazonaws.com:9000/ws";
+		"ws://ec2-54-149-65-170.us-west-2.compute.amazonaws.com:9000/ws";
 	//Constants
-	const script = 'My Voice Print recognizes me.'
+	const script =
+		"The Master of Business Administration degree originated in the United States in the early 20th century when the country industrialized and companies sought scientific approaches to management. The core courses in an MBA program cover various areas of business such as accounting, applied statistics, business communication, business ethics, business law, finance, managerial economics, management, entrepreneurship, marketing and operations in a manner most relevant to management analysis and strategy. Most programs also include elective courses and concentrations for further study in a particular area, for example accounting, finance, and marketing. MBA programs in the United States typically require completing about sixty credits, nearly twice the number of credits typically required for degrees that cover some of the same material such as the Master of Economics, Master of Finance, Master of Accountancy, Master of Science in Marketing and Master of Science in Management, The MBA is a terminal degree and a professional degree. Accreditation bodies specifically for MBA programs ensure consistency and quality of education. Business schools in many countries offer programs tailored to full-time, part-time, executive and distance learning students, many with specialized concentrations.";
 	//vars
 	const socket = useRef(undefined);
-	const transcriptRef = useRef(undefined);
-	const [bestTranscript,setBestTranscript] = useState({
+	const transcriptRef = useRef({
 		transcript:'',
 		time: 0,
 		stability:0
 	});
-	const [segments, setSegments] = useState({ start: script, middle: '', end: '' });
+	const [segments, setSegments] = useState([{string:script, match:false}]);
+	
 	useEffect(() => {
-		socket.current = new WebSocket(WEBSOCKET_URL);
-		socket.current.onmessage = e => {
-			let data = JSON.parse(e.data);
-			let transcript, timeObj, stability;
-			if (data.is_final === true) {
-				transcript = data.alternatives[0].transcript;
-				timeObj = data.result_end_time;
-				stability = 1;
-			} else {
-				transcript = data.alternatives[0].transcript;
-				timeObj = data.result_end_time;
-				stability = data.stability;
-			}
+		if (websocket) {
+			socket.current = new WebSocket(WEBSOCKET_URL);
+			socket.current.onmessage = e => {			
+				let data = JSON.parse(e.data);
+				let transcript, timeObj, stability;
+				if (data.is_final === true) {
+					transcript = data.alternatives[0].transcript;
+					timeObj = data.result_end_time;
+					stability = 1;
+				} else {
+					transcript = data.alternatives[0].transcript;
+					timeObj = data.result_end_time;
+					stability = data.stability;
+				}
 
-			if (transcript) {
-				let nanos = timeObj.nanos / 1000000000;
-				let seconds = timeObj.seconds || 0;
-				let time = seconds + nanos;
+				if (transcript) {
+					let nanos = timeObj.nanos / 1000000000;
+					let seconds = timeObj.seconds || 0;
+					let time = seconds + nanos;
 
-				if (transcriptRef.current.time < time) {
-					if (transcriptRef.current.stability <= stability) {
-						if (
-							transcriptRef.current.transcript.length <
-							transcript.length
-						) {
-							setBestTranscript({
-								transcript,
-								time,
-								stability
-							});
+					if (transcriptRef.current.time < time) {
+						if (transcriptRef.current.stability <= stability) {
+							if (
+								transcriptRef.current.transcript.length <
+								transcript.length
+							) {
+								transcriptRef.current = {
+									transcript,
+									time,
+									stability
+								};
+								let segments = searchStrings(
+									script,
+									transcript,
+									6
+								);
+								console.log({
+									segments
+								});
+								setSegments(segments);
+							}
 						}
 					}
 				}
-			}
-		};
-		socket.current.onopen = e => {
-			console.log({ message: "Connection Established", e });
-		};
-		socket.current.onerror = e => {
-			console.log({ message: "Error", e });
-		};
-		console.log(socket.current);
-		return () => {
-			socket.current.close();
-		};
+			};
+			socket.current.onopen = e => {
+				console.log({ message: "Connection Established", e });
+			};
+			socket.current.onerror = e => {
+				console.log({ message: "Error", e });
+			};
+			console.log(socket.current);
+			return () => {
+				socket.current.close();
+			};
+		}
 	},[]);
 
-	useEffect(() => {
-		let match = searchStrings(script,bestTranscript.transcript)
-		console.log(match)
-		setSegments(match.segments)
-		transcriptRef.current = bestTranscript
-	}, [bestTranscript]);
-
 	const handleFile = file => {
-		if (download === true) {
+		if (download) {
 			clientDownload(file);
 		}
-		if (s3Upload === true) {
+		if (s3Upload) {
 			S3UploadSinglePart(file, SIGNED_URL_ENDPOINT);
 		}
-		if (websocket === true) {	
+		if (websocket) {	
 			if (socket.current.readyState) {
 				socket.current.send(file);
 			}
@@ -96,16 +101,20 @@ function App() {
 			}
 		}
 	};
-
+	const displayScript = segments.map((item,index) =>{
+		let isLast = false;
+		if (index === segments.length-1) isLast = true;
+		return (
+			<span className={item.match ? "MatchText" : "UnmatchedText"}>
+				{item.string}{isLast ? null : "\u00A0"}
+			</span>
+		); 
+	})
 	return (
 		<div className='App'>
 			<h1>{"App"}</h1>
 			<div className={"ScriptContainer"}>
-				<p className={"UnmachedText"}>{segments.start || null}</p>
-				{segments.start ? "\u00A0" : null}
-				<p className={"MatchText"}>{segments.middle || null}</p>
-				{segments.middle ? "\u00A0" : null}
-				<p className={"UnmatchedText"}>{segments.end || null}</p>
+				<p>{displayScript}</p>
 			</div>
 			<AudioRecorder
 				channelNumber={1}
--- in-browser-recording-frontend/src/Modules/SearchStrings.js
+++ in-browser-recording-frontend/src/Modules/SearchStrings.js
@@ -1,40 +1,117 @@
-export const searchStrings = (mainString,compareString) => {
-    let match = false
-    console.log({main: mainString, compare: compareString});
-    let segments = {
-        start: mainString,
-        middle: '',
-        end: ''
-
-    }
-    if (mainString && compareString) {
-        if (mainString.length === compareString.length) {
-			if (mainString.toUpperCase() === compareString.toUpperCase()) {
-				match = true;
-				segments = {
-					start: "",
-                    middle: mainString,
-                    end: ''
-				};
-			}
-		} else {
-			for (let i = 0; i < mainString.length - compareString.length; i++) {
-				let segment = mainString.slice(i, i + compareString.length);
-				if (segment.toUpperCase() === compareString.toUpperCase()) {
-					match = true;
-					segments = {
-						start: mainString.slice(0, i),
-						middle: mainString.slice(i, i + compareString.length),
-						end: mainString.slice(
-							i + compareString.length,
-							mainString.length
-						)
-                    };
-                    return { match, segments };
+//Diff algorithm
+export const searchStrings = (mainString,compareString,adjacency) => {
+	console.log({mainString,compareString})
+	//Break Strings into words
+	let mainStringWords = getWords(mainString);
+	let compareStringWords = getWords(compareString);
+	console.log({mainString: mainStringWords, compareString:compareStringWords})
+	//Find Matches
+	let matches = [];
+	for (let word = 0; word < compareStringWords.length; word++) {
+		for (let offset = -adjacency; offset <= adjacency; offset ++) {
+			let target = word + offset
+			if (target >= 0 && target < mainStringWords.length) {
+				if (
+					compareStringWords[word].toUpperCase() ===
+					mainStringWords[target].toUpperCase()
+				) {
+					if (Math.abs(offset) !== 0) {
+						matches.push({
+							word: mainStringWords[target],
+							strength: 1 - Math.abs(offset) * (1 / (adjacency + 1)),
+							index: target
+						});
+					} else {
+						matches.push({
+							word: mainStringWords[target],
+							strength: 1,
+							index: target
+						});
+					}
 				}
 			}
 		}
-    }
-    
-    return {match, segments}
+	}
+	//Filter Matches
+	let bestMatches = []
+	for (let i = 0; i < mainStringWords.length; i++) {
+		bestMatches.push({strength:0})
+	}
+	for (let i = 0; i < mainStringWords.length; i++) {
+		for (let j = 0; j < matches.length; j++) {
+			if (matches[j].index === i) {
+				if (bestMatches[i].strength < matches[j].strength) {
+					bestMatches[i] = matches[j];
+				}
+			}
+		}
+	}
+	//Build String
+	let strings = []
+	let stringCount = 0;
+	let matchStringActive = false;
+	let noMatchStringActive = false;
+	for (let i = 0; i < bestMatches.length; i++) {
+		if(bestMatches[i].word) {
+			if (matchStringActive) {
+				strings[stringCount].string = strings[
+					stringCount
+				].string.concat(" ", bestMatches[i].word);
+			}
+			else {
+				if (noMatchStringActive) {
+					stringCount++;
+					noMatchStringActive = false;
+				}
+				strings.push({string: bestMatches[i].word, match: true})
+				matchStringActive = true;				
+			}
+		}
+		else {
+			if (matchStringActive) {
+				matchStringActive = false
+				stringCount++;
+				strings.push({
+					string: mainStringWords[i],
+					match: false
+				});
+				noMatchStringActive = true
+			}
+			else {
+				if (noMatchStringActive) {
+					strings[stringCount].string = strings[
+						stringCount
+					].string.concat(" ", mainStringWords[i]);
+				}
+				else {
+					strings.push({
+						string: mainStringWords[i],
+						match: false
+					});
+					noMatchStringActive = true;
+				}
+				
+			}
+		}
+	}
+	return strings;
+}
+
+const getWords = (string) => {
+	let lastChar = "";
+	let lastWord = 0;
+	let stringWords = [];
+	for (let i = 0; i <= string.length; i++) {
+		if (
+			lastChar !== " " &&
+			(string.charAt(i) === " " || i === string.length)
+		) {
+			let word = string.slice(lastWord, i);
+			stringWords.push(word);
+		} else if (lastChar === " " && string.charAt(i) !== " ") {
+					lastWord = i;
+				}
+		lastChar = string.charAt(i);
+	}
+	return stringWords;
 }
